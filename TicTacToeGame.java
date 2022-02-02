bba8d87287b7df1ce92770abe3ea5b68aecdd3b1
w10010010 <kevin2003yu@gmail.com>
2022-02-01 22:24:22 -0500

    dsaf

---
+++ TicTacToeGame.java
@@ -0,0 +1,227 @@
+/**
+ * The class <b>TicTacToeGame</b> is the
+ * class that implements the Tic Tac Toe Game.
+ * It contains the grid and tracks its progress.
+ * It automatically maintain the current state of
+ * the game as players are making moves.
+ *
+ * @author Guy-Vincent Jourdan, University of Ottawa
+ */
+public class TicTacToeGame {
+
+   /**
+	* The board of the game, stored as a single array.
+	*/
+	private CellValue[] board;
+
+
+   /**
+	* level records the number of rounds that have been
+	* played so far. Starts at 0.
+	*/
+	private int level;
+
+   /**
+	* gameState records the current state of the game.
+	*/
+	private GameState gameState;
+
+
+   /**
+	* lines is the number of lines in the grid
+	*/
+	private final int lines;
+
+   /**
+	* columns is the number of columns in the grid
+	*/
+	private final int columns;
+
+
+   /**
+	* sizeWin is the number of cell of the same type
+	* that must be aligned to win the game. 
+	* For simplicity, it will be always 3 in this assignment.
+	*/
+	private final int sizeWin; 
+
+
+   /**
+	* default constructor, for a game of 3x3, which must
+	* align 3 cells
+	*/
+	public TicTacToeGame(){
+		// your code here
+	}
+
+   /**
+	* constructor allowing to specify the number of lines
+	* and the number of columns for the game. 3 cells must
+	* be aligned.
+   	* @param lines
+    *  the number of lines in the game
+    * @param columns
+    *  the number of columns in the game
+  	*/
+	public TicTacToeGame(int lines, int columns){
+		// your code here
+	}
+
+   /**
+	* constructor allowing to specify the number of lines
+	* and the number of columns for the game, as well as
+	* the number of cells that must be aligned to win.
+   	* @param lines
+    *  the number of lines in the game
+    * @param columns
+    *  the number of columns in the game
+    * @param sizeWin
+    *  the number of cells that must be aligned to win.
+  	*/
+	public TicTacToeGame(int lines, int columns, int sizeWin){
+		// your code here
+	}
+
+
+
+   /**
+	* getter for the variable lines
+	* @return
+	* 	the value of lines
+	*/
+	public int getLines(){
+		// your code here
+	}
+
+   /**
+	* getter for the variable columns
+	* @return
+	* 	the value of columns
+	*/
+	public int getColumns(){
+		// your code here
+	}
+
+   /**
+	* getter for the variable level
+	* @return
+	* 	the value of level
+	*/
+	public int getLevel(){
+		// your code here
+	}
+
+
+   /**
+	* getter for the variable gameState
+	* @return
+	* 	the value of gameState
+	*/
+	public GameState getGameState(){
+		// your code here
+	}
+
+   /**
+	* getter for the variable sizeWin
+	* @return
+	* 	the value of sizeWin
+	*/
+	public int getSizeWin(){
+		// your code here
+	}
+
+   /**
+	* returns the cellValue that is expected next,
+	* in other word, which played (X or O) should
+	* play next.
+	* This method does not modify the state of the
+	* game.
+	* @return
+    *  the value of the enum CellValue corresponding
+    * to the next expected value.
+  	*/
+	public CellValue nextCellValue(){
+		// your code here
+	}
+
+   /**
+	* returns the value  of the cell at
+	* index i.
+	* If the index is invalid, an error message is
+	* printed out. The behaviour is then unspecified
+   	* @param i
+    *  the index of the cell in the array board
+    * @return
+    *  the value at index i in the variable board.
+  	*/
+	public CellValue valueAt(int i) {
+		// your code here
+	}
+
+   /**
+	* This method is called by the next player to play
+	* at the cell  at index i.
+	* If the index is invalid, an error message is
+	* printed out. The behaviour is then unspecified
+	* If the chosen cell is not empty, an error message is
+	* printed out. The behaviour is then unspecified
+	* If the move is valide, the board is updated, as well
+	* as the state of the game.
+	* To faciliate testing, it is acceptable to keep playing
+	* after a game is already won. If that is the case, the
+	* a message should be printed out and the move recorded.
+	* the  winner of the game is the player who won first
+   	* @param i
+    *  the index of the cell in the array board that has been
+    * selected by the next player
+  	*/
+	public void play(int i) {
+
+		// your code here
+
+	}
+
+
+   /**
+	* A helper method which updates the gameState variable
+	* correctly after the cell at index i was just set.
+	* The method assumes that prior to setting the cell
+	* at index i, the gameState variable was correctly set.
+	* it also assumes that it is only called if the game was
+	* not already finished when the cell at index i was played
+	* (the the game was playing). Therefore, it only needs to
+	* check if playing at index i has concluded the game
+	* So check if 3 cells are formed to win.
+//   	* @param i
+    *  the index of the cell in the array board that has just
+    * been set
+  	*/
+
+	private void setGameState(int index){
+
+		// your code here
+		
+
+	}
+
+
+
+	final String NEW_LINE = System.getProperty("line.separator");
+	// returns the OS dependent line separator
+
+   /**
+	* Returns a String representation of the game matching
+	* the example provided in the assignment's description
+	*
+   	* @return
+    *  String representation of the game
+  	*/
+
+	public String toString(){
+		// your code here
+		// use NEW_LINE defined above rather than \n
+		
+
+	}
+
+}
